c6741f7577440eef932479ce6ebde36e74b8b999
ephraimaondofa-maker <ephraimaondofa@gmail.com>
2026-07-15 11:49:23 +0100

    Add files via upload

---
+++ firstprogramme.py
@@ -0,0 +1,40 @@
+
+# MY DAY ONE PYTHON EXERCISE
+# PROGRAMME TO PRINT MY NAME 
+
+print("Welcome to python!\n")
+
+print("My name is: Ikyaaibo Ephraim Aondofa")
+print("My favorite food is: Swallow with Egusi")
+
+print('\n')
+"""
+Printing my name, age and country using different print statements
+"""
+print('First method using only print function \n*********************************')
+
+print("Name: Ephraim Aondofa \n")
+print("Age: 28 \n")
+print("Country: Nigeria \n")
+
+print('Second method using variables\n************************************')
+
+name = ("Ikyaaibo Ephraim Aondofa")
+age = (28)
+Country = ("Nigeria")
+print("End of name, age and country  programme")
+
+"""
+Reproducing expected output
+name 
+career
+country
+"""
+print("********************")
+print("      MY PROFILE")
+print("********************")
+print("Name : Ephraim ")
+print("Career : AI Engineer ")
+print("Country : Nigeria")
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+python programme to print about me
+
+Your name
+Your career goal
+Why you want to learn AI engineering
+One skill you want to master this year
+"""
+print("***** ABOUT ME*****")
+print("Name: Ikyaaibo Ephraim Aondofa")
+print("Career Goal: AI Engineering")
+print("I i love AI Engineering: To become a problem solver in software dev.")
+print("Skill to Master this year: Python for WebDev.")
+
